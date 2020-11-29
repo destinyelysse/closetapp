253da86cc9b6b396aff65087c0ebbb1c75cbd2f6
Destiny Serna <destinyeserna@gmail.com>
2020-11-28 18:07:39 -0800

    Initial Commit

---
+++ Closet/ClosetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClosetApp.swift
+//  Closet
+//
+//  Created by Destiny Serna on 11/28/20.
+//
+
+import SwiftUI
+
+@main
+struct ClosetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
